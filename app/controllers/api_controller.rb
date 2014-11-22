e3c2f733a49333c510f84fd7ad78be8dbc6fee62
Benji dalton <daylightsavings@gmail.com>
2014-11-23 01:55:28 +1300

    API basic security enabled

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  http_basic_authenticate_with name: "admin", password: "secret"
 
-  # necessary in all controllers that will respond with JSON
   respond_to :json
 
   private
