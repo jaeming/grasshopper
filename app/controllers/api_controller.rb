1ca593bc0110cbe3781823b855f429777f7a80d6
Benji dalton <daylightsavings@gmail.com>
2014-12-12 01:28:48 +1300

    added cors headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,21 @@
 class ApiController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  # http_basic_authenticate_with name: "admin", password: "secret", except: [:index, :show]
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   respond_to :json
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+   def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
 
   # def default_serializer_options
   #   {root: false}
