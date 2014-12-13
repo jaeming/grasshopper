39e19c3fcfda56d0bfb9ce35725e607e83273a75
Benji dalton <daylightsavings@gmail.com>
2014-12-14 02:40:48 +1300

    fixes search duplicates

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,8 +2,9 @@ class Search
   def self.for(query)
     query_search = "%#{query.downcase}%"
 
-    Board.where('lower(title) LIKE ?', query_search) +
-    Board.where('lower(text) LIKE ?', query_search) +
+    # Board.where('lower(title) LIKE ?', query_search) +
+    # Board.where('lower(text) LIKE ?', query_search) +
+    Board.where('lower(title) LIKE ? or lower(text) LIKE ?', query_search, query_search) +
     Message.where('lower(body) LIKE ?', query_search)
   end
 end
