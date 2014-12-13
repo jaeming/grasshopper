896d0c15f690fa6c928f6c19da3fd9613dea82c7
Benji <benji@nonentity.local>
2014-12-14 03:19:42 +1300

    refactors Search class

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,9 +2,7 @@ class Search
   def self.for(query)
     query_search = "%#{query.downcase}%"
 
-    # Board.where('lower(title) LIKE ?', query_search) +
-    # Board.where('lower(text) LIKE ?', query_search) +
-    Board.where('lower(title) LIKE ? or lower(text) LIKE ?', query_search, query_search) +
+    Board.where('lower(title) LIKE :query or lower(text) LIKE :query', { query: query_search }) +
     Message.where('lower(body) LIKE ?', query_search)
   end
 end
