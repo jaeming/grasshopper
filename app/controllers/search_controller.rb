3f9f635e57967287c1d6f974d938e1609d3950de
Benji Dalton <daylightsavings@gmail.com>
2015-01-08 20:52:57 +1300

    refactors Search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,20 +2,14 @@ class SearchController < ApiController
 respond_to :json
 
   def index
-    if params[:q].present?
-      @board_title = Board.search(search_params).map { |r| r._source}
-      @message_body = Message.search(search_params).map { |r| r._source}
-      @search = @board_title + @message_body
-      render json: @search, root: false
-    else
-      @search = []
-    end
+    @results = query(Board) + query(Message)
+    render json: @results, root: false
   end
 
   private
 
-  def search_params
-    params[:q].split.join(' AND ')
+  def query(klass)
+    klass.search(params[:q]).map { |r| r._source }
   end
 
 end
